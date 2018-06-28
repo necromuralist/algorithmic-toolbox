24a7a3db0c2594fd366b5984b4b5bd86ad8aca3d
necromuralist <necromuralist@protonmail.com>
2018-06-27 19:10:59 -0700

    Working on pisano cycles

--- algorithmic_toolbox/implementations.py
+++ algorithmic_toolbox/implementations.py
@@ -10,3 +10,20 @@ def greatest_common_divisor(a, b):
     while b != 0:
         a, b = b, a % b
     return a
+
+def fibonacci(n):
+    """Calculates the nth fibonacci number
+
+    Args:
+     n (int): the fibonacci number to get (e.g. 3 means third)
+
+    Returns:
+     int: nth fibonacci number
+    """
+    first = (0, 1)
+    if n in first:
+        return n
+    previous, current = first
+    for index in range(2, n + 1):
+        previous, current = current, previous + current
+    return current

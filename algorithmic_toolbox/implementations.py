b1e1f79c943fc3439bd14ebf8ce4165ff37100e8
dogen <necromuralist@protonmail.com>
2018-07-01 17:39:07 -0700

    Added large fibonacci numbers function

--- algorithmic_toolbox/implementations.py
+++ algorithmic_toolbox/implementations.py
@@ -1,3 +1,6 @@
+# from pypi
+from joblib import Memory
+
 def greatest_common_divisor(a, b):
     """finds the GCD of a and b
 
@@ -12,6 +15,26 @@ def greatest_common_divisor(a, b):
     return a
 
 def fibonacci(n):
+    """Calculates the nth fibonacci number
+
+    Args:
+     n (int): the fibonacci number to get (e.g. 3 means third)
+
+    Returns:
+     int: nth fibonacci number
+    """
+    first = (0, 1)
+    if n in first:
+        return n
+    previous, current = first
+    for index in range(2, n + 1):
+        previous, current = current, previous + current
+    return current
+
+memory = Memory(location="/tmp/memories/")
+
+@memory.cache
+def fibonacci_cached(n):
     """Calculates the nth fibonacci number
 
     Args:
